cc715da79dfbcbdab57d7fd441cc8266ab9ac071
dorian1000 <62339904+dorian1000@users.noreply.github.com>
2021-01-05 17:00:59 +0100

    updated to v0.0.2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "♥ Hello, i am the demo app for the clc_flagger_project. My Version is 0.0.1 ♥")
+	fmt.Fprintln(w, "♥ Hello again, i am the demo app for the clc_flagger_project. My Version is 0.0.2 ♥")
 }
 
 func main() {
